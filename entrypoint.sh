906dc14b4dd2f89c7955ebd34e8e8ef70afe2a90
Neeraj Lamsal <itskop520@gmail.com>
2022-08-12 18:32:06 +0545

    added swagger docs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-yarn dev
+yarn start:dev
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('streammmm')
+    .setDescription('Backend of streammmm API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+  console.log(`server started on http://localhost:8000`);
 }
 
 bootstrap();
