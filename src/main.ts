9dc24226f9841ea771acf6e57d4e2fedd4cee170
Neeraj Lamsal <itskop520@gmail.com>
2022-08-19 10:54:26 +0545

    [added] morgan for logging

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(morgan('tiny'));
   const config = new DocumentBuilder()
     .setTitle('streammmm')
     .setDescription('Backend of streammmm API')
@@ -15,8 +19,8 @@ async function bootstrap() {
 
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000)
-    
+  await app.listen(3000);
+
   console.log(`server started on http://localhost:8000`);
 }
 
